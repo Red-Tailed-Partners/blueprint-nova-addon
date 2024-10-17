a7552fd0091efed5da233874eec4ac58158d571b
John Sykes <john@sykes.org>
2024-10-17 11:21:45 -0700

    Correct Nova Resource Class Name

--- src/NovaGenerator.php
+++ src/NovaGenerator.php
@@ -77,7 +77,7 @@ class NovaGenerator implements Generator
 
         $stub = str_replace('DummyNamespace', $resourceNamespace, $stub);
         $stub = str_replace('id', $model->primaryKey(), $stub);
-        $stub = str_replace('DummyClass', $model->name(), $stub);
+        $stub = str_replace('DummyClass', $model->name().'Resource', $stub);
         $stub = str_replace('DummyModel', '\\'.$model->fullyQualifiedClassName(), $stub);
         $stub = str_replace('// fields...', $data['fields'], $stub);
         $stub = str_replace('use Laravel\Nova\Http\Requests\NovaRequest;', implode(PHP_EOL, $data['imports']), $stub);
